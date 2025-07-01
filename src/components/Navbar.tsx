06ede6c1e90a82c62986faceda5805fad48ec131
BIANCA_TAKAMORI <biancahelenatakamoriribeiro@gmail.com>
2025-07-01 14:24:23 -0300

    add pathname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -10,6 +11,7 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+  const pathname = usePathname();
 
   return (
     <nav className="fixed top-0 w-full bg-white shadow-md z-50 border border-b-[#A8B5A2]">
@@ -29,25 +31,35 @@ const Navbar = () => {
             <div className="ml-10 flex items-center space-x-8">
               <Link
                 href="/"
-                className="text-gray-600 hover:text-gray-800 transition-colors duration-200"
+                className={`hover:text-gray-800 transition-colors duration-200 ${
+                  pathname === "/" ? "text-gray-400" : "text-gray-600"
+                }`}
               >
                 Home
               </Link>
               <Link
                 href="/collections"
-                className="text-gray-600 hover:text-gray-800 transition-colors duration-200"
+                className={`hover:text-gray-800 transition-colors duration-200 ${
+                  pathname === "/collections"
+                    ? "text-gray-400"
+                    : "text-gray-600"
+                }`}
               >
                 Coleções
               </Link>
               <Link
                 href="/about"
-                className="text-gray-600 hover:text-gray-800 transition-colors duration-200"
+                className={`hover:text-gray-800 transition-colors duration-200 ${
+                  pathname === "/about" ? "text-gray-400" : "text-gray-600"
+                }`}
               >
                 Sobre
               </Link>
               <Link
                 href="/shop"
-                className="text-gray-200 transition-colors duration-200 bg-[#A8B5A2] p-2 rounded-md shadow-md hover:bg-[#A8B5A2]/80"
+                className={`transition-colors duration-200 bg-[#A8B5A2] p-2 rounded-md shadow-md hover:bg-[#A8B5A2]/80 ${
+                  pathname === "/shop" ? "text-gray-300" : "text-gray-100"
+                }`}
               >
                 Shop
               </Link>
@@ -81,28 +93,36 @@ const Navbar = () => {
         <div className="px-4 pt-2 pb-4 space-y-4 bg-white-50 shadow-inner">
           <Link
             href="/"
-            className="block text-gray-600 hover:text-gray-800 px-3 py-2 rounded-md transition-colors duration-200"
+            className={`block px-3 py-2 rounded-md ${
+              pathname === "/" ? "text-gray-400" : "text-gray-600"
+            }`}
             onClick={toggleMenu}
           >
             Home
           </Link>
           <Link
             href="/collections"
-            className="block text-gray-600 hover:text-gray-800 px-3 py-2 rounded-md transition-colors duration-200"
+            className={`block px-3 py-2 rounded-md ${
+              pathname === "/collections" ? "text-gray-400" : "text-gray-600"
+            }`}
             onClick={toggleMenu}
           >
             Coleções
           </Link>
           <Link
             href="/about"
-            className="block text-gray-600 hover:text-gray-800 px-3 py-2 rounded-md transition-colors duration-200"
+            className={`block px-3 py-2 rounded-md ${
+              pathname === "/about" ? "text-gray-400" : "text-gray-600"
+            }`}
             onClick={toggleMenu}
           >
             Sobre
           </Link>
           <Link
             href="/shop"
-            className="block text-white bg-[#A8B5A2] px-3 py-2 rounded-md transition-colors duration-200"
+            className={`block bg-[#A8B5A2] px-3 py-2 rounded-md transition-colors duration-200 ${
+              pathname === "/shop" ? "text-gray-300" : "text-white"
+            }`}
             onClick={toggleMenu}
           >
             Shop
